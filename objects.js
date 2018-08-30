fa168af41c267ff863540b756be94bb4962cdf53
Oshi <joshkap2015@gmail.com>
2018-08-30 10:00:57 +0200

    Hello

---
+++ objects.js
@@ -0,0 +1,25 @@
+const article = {
+  title: "Trump marries someone",
+  dateOfCreation:
+}
+
+let books = [
+  {
+    name: "JavaScript & jQuery",
+    author: "John Duckett",
+    released: {
+      year: 2014,
+      month: 8
+    },
+    reviews: [
+      {
+        author: "Hansi",
+        rating: 8
+      },
+      {
+        author: "Po",
+        rating: 6
+      }
+    ]
+  }
+]
